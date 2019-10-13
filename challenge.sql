68119e76f85e618384a8c412110123467abb4e1f
JOSHUA EVANS <102909073@student.swin.edu.au>
2019-10-14 09:19:32 +1100

    Task 2 Complete

--- challenge.sql
+++ challenge.sql
@@ -1,4 +1,6 @@
 /* 
+102909073
+
 Tour (TourName, Description)
 Primary Key(TourName)
 
@@ -13,8 +15,46 @@ Foreign Key (ClientID) References Client
 Client(ClientID, Surname, GivenName, Gender)
 PrimaryKey(ClientID)
 */
-
+DROP TABLE IF EXISTS dbo.Booking
+DROP TABLE IF EXISTS dbo.Event
 DROP TABLE IF EXISTS dbo.Tour
 DROP TABLE IF EXISTS dbo.Client
-DROP TABLE IF EXISTS dbo.Event
-DROP TABLE IF EXISTS dbo.Booking
+
+
+
+CREATE TABLE Tour(
+    TourName    NVARCHAR(100)
+,   Description NVARCHAR(500)
+,   PRIMARY Key(TourName)
+);
+
+CREATE TABLE Client(
+    Clientid    INT
+,   Surname     NVARCHAR(100)
+,   GivenName   NVARCHAR(100)
+,   Gender      NVARCHAR(1)
+,   PRIMARY KEY(ClientID)
+);
+
+CREATE TABLE Event(
+    TourName    NVARCHAR(100)
+,   EventMonth  NVARCHAR(3)
+,   EventDay    INT
+,   EventYear   INT
+,   EventFee    Money
+,   PRIMARY KEY(TourName, EventMonth, EventDay, EventYear)
+,   FOREIGN KEY(TourName) REFERENCES Tour
+);
+
+CREATE TABLE Booking(
+    Clientid    INT FOREIGN KEY REFERENCES Client
+,   TourName    NVARCHAR(100)
+,   EventMonth  NVARCHAR(3)
+,   EventDay    INT
+,   EventYear   INT
+,   Payment     Money
+,   DateBooked  DATE
+,   PRIMARY KEY (TourName, EventMonth, EventDay, EventYear)
+,   FOREIGN KEY (TourName, EventMonth, EventDay, EventYear) REFERENCES Event
+);
+
