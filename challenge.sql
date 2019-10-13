a6e914e47795627aa1a778922d73cf906f5cc05e
JOSHUA EVANS <102909073@student.swin.edu.au>
2019-10-14 10:15:25 +1100

    Task 3 Fixed and Complete

--- challenge.sql
+++ challenge.sql
@@ -20,8 +20,6 @@ DROP TABLE IF EXISTS dbo.Event
 DROP TABLE IF EXISTS dbo.Tour
 DROP TABLE IF EXISTS dbo.Client
 
-
-
 CREATE TABLE Tour(
     TourName    NVARCHAR(100)
 ,   Description NVARCHAR(500)
@@ -32,13 +30,13 @@ CREATE TABLE Client(
     Clientid    INT
 ,   Surname     NVARCHAR(100) NOT NULL
 ,   GivenName   NVARCHAR(100) NOT NULL
-,   Gender      NVARCHAR(1) CHECK(Gender = "M" OR Gender = "G" OR Gender = "I")
+,   Gender      NVARCHAR(1) CHECK(Gender = 'M' OR Gender = 'F' OR Gender = 'I')
 ,   PRIMARY KEY(ClientID)
 );
 
 CREATE TABLE Event(
     TourName    NVARCHAR(100)
-,   EventMonth  NVARCHAR(3) CHECK (EventMonth = "Jan" OR EventMonth = "Feb" OR EventMonth = "Mar" OR EventMonth = "Apr" OR EventMonth = "May" OR EventMonth = "Jun" OR EventMonth = "Jul" OR EventMonth = "Aug" OR EventMonth = "Sep" OR EventMonth = "Oct" OR EventMonth = "Nov" OR EventMonth = "Dec")
+,   EventMonth  NVARCHAR(3) CHECK (EventMonth = 'Jan' OR EventMonth = 'Feb' OR EventMonth = 'Mar' OR EventMonth = 'Apr' OR EventMonth = 'May' OR EventMonth = 'Jun' OR EventMonth = 'Jul' OR EventMonth = 'Aug' OR EventMonth = 'Sep' OR EventMonth = 'Oct' OR EventMonth = 'Nov' OR EventMonth = 'Dec')
 ,   EventDay    INT CHECK (EventDay >= 1 OR EventDay <= 31)
 ,   EventYear   INT CHECK (Len(EventYear) = 4)
 ,   EventFee    Money NOT NULL CHECK (EventFee > 0)
@@ -49,37 +47,38 @@ CREATE TABLE Event(
 CREATE TABLE Booking(
     Clientid    INT FOREIGN KEY REFERENCES Client
 ,   TourName    NVARCHAR(100)
-,   EventMonth  NVARCHAR(3) CHECK (EventMonth = "Jan" OR EventMonth = "Feb" OR EventMonth = "Mar" OR EventMonth = "Apr" OR EventMonth = "May" OR EventMonth = "Jun" OR EventMonth = "Jul" OR EventMonth = "Aug" OR EventMonth = "Sep" OR EventMonth = "Oct" OR EventMonth = "Nov" OR EventMonth = "Dec")
+,   EventMonth  NVARCHAR(3) CHECK (EventMonth = 'Jan' OR EventMonth = 'Feb' OR EventMonth = 'Mar' OR EventMonth = 'Apr' OR EventMonth = 'May' OR EventMonth = 'Jun' OR EventMonth = 'Jul' OR EventMonth = 'Aug' OR EventMonth = 'Sep' OR EventMonth = 'Oct' OR EventMonth = 'Nov' OR EventMonth = 'Dec')
 ,   EventDay    INT CHECK (EventDay >= 1 OR EventDay <= 31)
 ,   EventYear   INT CHECK (Len(EventYear) = 4)
-,   Payment     Money CHECK (EventFee > 0)
+,   Payment     Money CHECK (Payment > 0)
 ,   DateBooked  DATE NOT NULL
-,   PRIMARY KEY (TourName, EventMonth, EventDay, EventYear)
+,   PRIMARY KEY (Clientid, TourName, EventMonth, EventDay, EventYear)
 ,   FOREIGN KEY (TourName, EventMonth, EventDay, EventYear) REFERENCES Event
 );
 
-INSERT INTO Tour (TourName, Description) VALUES (North, "Tour of wineries and outlets of the Bedigo and Castlemaine region");
-INSERT INTO Tour (TourName, Description) VALUES (South, "Tour of wineries and outlets of Mornington Penisula");
-INSERT INTO Tour (TourName, Description) VALUES (West, "Tour of wineries and outlets of the Geelong and Otways region");
+INSERT INTO Tour (TourName, Description) VALUES ('North', 'Tour of wineries and outlets of the Bedigo and Castlemaine region');
+INSERT INTO Tour (TourName, Description) VALUES ('South', 'Tour of wineries and outlets of Mornington Penisula');
+INSERT INTO Tour (TourName, Description) VALUES ('West', 'Tour of wineries and outlets of the Geelong and Otways region');
 
-INSERT INTO Client (Clientid, Surname, GivenName, Gender) VALUES (1, "Price", "Taylor", "M");
-INSERT INTO Client (Clientid, Surname, GivenName, Gender) VALUES (2, "Gamble", "Ellyse", "F");
-INSERT INTO Client (Clientid, Surname, GivenName, Gender) VALUES (3, "Tan", "Tilly", "F");
+INSERT INTO Client (Clientid, Surname, GivenName, Gender) VALUES (1, 'Price', 'Taylor', 'M');
+INSERT INTO Client (Clientid, Surname, GivenName, Gender) VALUES (2, 'Gamble', 'Ellyse', 'F');
+INSERT INTO Client (Clientid, Surname, GivenName, Gender) VALUES (3, 'Tan', 'Tilly', 'F');
 
-INSERT INTO Event (TourName, EventMonth, EventDay, EventFee) VALUES ("North", "Jan", 9, 2016, 200);
-INSERT INTO Event (TourName, EventMonth, EventDay, EventFee) VALUES ("North", "Feb", 13, 2016, 225);
-INSERT INTO Event (TourName, EventMonth, EventDay, EventFee) VALUES ("South", "Jan", 9, 2016, 200);
-INSERT INTO Event (TourName, EventMonth, EventDay, EventFee) VALUES ("South", "Jan", 16, 2016, 200);
-INSERT INTO Event (TourName, EventMonth, EventDay, EventFee) VALUES ("West", "Jan", 29, 2016, 225);
+INSERT INTO Event (TourName, EventMonth, EventDay, EventYear, EventFee) VALUES ('North', 'Jan', 9, 2016, 200);
+INSERT INTO Event (TourName, EventMonth, EventDay, EventYear, EventFee) VALUES ('North', 'Feb', 13, 2016, 225);
+INSERT INTO Event (TourName, EventMonth, EventDay, EventYear, EventFee) VALUES ('South', 'Jan', 9, 2016, 200);
+INSERT INTO Event (TourName, EventMonth, EventDay, EventYear, EventFee) VALUES ('South', 'Jan', 16, 2016, 200);
+INSERT INTO Event (TourName, EventMonth, EventDay, EventYear, EventFee) VALUES ('West', 'Jan', 29, 2016, 225);
 
-INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (1, "North", "Jan", 9, 2016, 200, 10/12/2015);
-INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (2, "North", "Jan", 9, 2016, 200, 16/12/2015);
-INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (1, "North", "Feb", 13, 2016, 225, 8/01/2016);
-INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (2, "North", "Feb", 13, 2016, 125, 14/01/2016);
-INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (3, "North", "Feb", 13, 2016, 225, 3/02/2016);
-INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (1, "South", "Jan", 9, 2016, 200, 10/12/2015);
-INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (2, "South", "Jan", 16, 2016, 200, 18/12/2015);
-INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (3, "South", "Jan", 16, 2016, 200, 9/01/2016);
-INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (2, "West", "Jan", 29, 2016, 225, 17/12/2015);
-INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (3, "West", "Jan", 29, 2016, 200, 18/12/2015);
+INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (1, 'North', 'Jan', 9, 2016, 200, '2015-12-10');
+INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (2, 'North', 'Jan', 9, 2016, 200, '2015-12-16');
+INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (1, 'North', 'Feb', 13, 2016, 225, '2016-01-08');
+INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (2, 'North', 'Feb', 13, 2016, 125, '2016-01-14');
+INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (3, 'North', 'Feb', 13, 2016, 225, '2016-02-03');
+INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (1, 'South', 'Jan', 9, 2016, 200, '2015-12-10');
+INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (2, 'South', 'Jan', 16, 2016, 200, '2015-12-18');
+INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (3, 'South', 'Jan', 16, 2016, 200, '2016-01-09');
+INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (2, 'West', 'Jan', 29, 2016, 225, '2015-12-17');
+INSERT INTO Booking (Clientid, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES (3, 'West', 'Jan', 29, 2016, 200, '2015-12-18');
 
+Select * from Client
